493827f293cd14b9181772d0c8f094cea708d595
Edgar Rojas <90846268+edgar2420@users.noreply.github.com>
2025-04-22 10:24:41 -0400

    Update formula.routes.js
    ruta de todas

--- routes/formula.routes.js
+++ routes/formula.routes.js
@@ -5,6 +5,7 @@ const controller = require("../controllers/formula.controller");
 router.get("/producto/:nombre", controller.obtenerFormulasPorProducto);
 router.post("/crear", controller.crearFormula);
 router.post("/importar-excel", controller.importarDesdeExcel);
+router.get("/todas", controller.obtenerTodasLasFormulas);
 
 module.exports = app => {
   app.use("/formulas", router);
